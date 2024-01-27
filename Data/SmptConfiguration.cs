bfefc9a7b45163033f87a42be2ba3fd46b2b8ba9
Kaede Kei <99081219+KaedeKei@users.noreply.github.com>
2024-01-27 23:47:27 +0300

    немного поправила SptpConfiguration

--- Data/SmptConfiguration.cs
+++ Data/SmptConfiguration.cs
@@ -1,31 +1,43 @@
-﻿using System.Reflection;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection;
 
 namespace BlazorShop.Data
 {
 	public class SmptConfiguration : ISmtpConfiguration
 	{
-		private IConfiguration _configuration = new ConfigurationBuilder().AddUserSecrets(Assembly.GetExecutingAssembly(), true).Build();
+		private IConfiguration _configuration;
+		[Required] private string Host;
+		[EmailAddress] private string UserName;
+		[Required] private string Password;
+		[Range(1, ushort.MaxValue)] private int Port;
+
+		public SmptConfiguration()
+		{
+			_configuration = new ConfigurationBuilder().AddUserSecrets(Assembly.GetExecutingAssembly(), true).Build();
+			Host = _configuration["Host"];
+			UserName = _configuration["UserName"];
+			Password = _configuration["Password"];
+			Port = Convert.ToInt32(_configuration["port"]);
+		}		
 
 		public string GetHost()
 		{
-			return _configuration["Host"];
+			return Host;
 		}
 
 		public string GetUserName()
 		{
-			return _configuration["UserName"];
+			return UserName;
 		}
 
 		public string GetPassword()
 		{
-			return _configuration["Password"];
+			return Password;
 		}
 
 		public int GetPort()
 		{
-			var port = _configuration["port"];
-
-			return Convert.ToInt32(port);
+			return Port;
 		}
 	}
 }
